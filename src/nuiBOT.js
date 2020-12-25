38a66ead54bba6c0bc7f37b01e49710c0d56e9d6
Jacky Xie <z5257079@cse.unsw.edu.au>
2020-12-25 17:24:36 +1100

    a

--- src/nuiBOT.js
+++ src/nuiBOT.js
@@ -19,30 +19,40 @@ client.on('message', message => {
 	if (message.author.bot) return;
 
 	let args = message.content.substring(PREFIX.length).split(" ");
+	function play(connection, message) {
+		var server = servers[message.guild.id];
+		const broadcast = client.voice.createBroadcast();
+
+		server.dispatcher = broadcast.play(ytdl(server.queue[0],{filter:"audioonly"}));
+		connection.play(broadcast);
+		server.queue.shift();
+
+		server.dispatcher.on("finish", ()=> {
+			if(server.queue[0]) {
+				console.log('continuing');
+				play(connection, message);
+			}else {
+				console.log('disconnect');
+				server.dispatcher.destroy();
+				connection.disconnect();
+				console.log(server.dispatcher.destroyed)
+				//make sure that the dispatcher object is removed from server
+				delete server.dispatcher;
+				console.log(servers[message.guild.id])
+				return;
+			}
+		})
+	}
 
 	switch(args[0]) {
 		case 'play':
-			function play(connection, message) {
-				var server = servers[message.guild.id];
-				
-				server.dispatcher = connection.play(ytdl(server.queue[0],{filter:"audioonly"}));
-				server.queue.shift();
 
-				server.dispatcher.on("end", ()=> {
-					if(server.queue[0]) {
-						play(connection, message);
-					}else {
-						connection.disconnect();
-					}
-				})
-			}
 
 			if (!args[1]) {
 				message.channel.send('please provide a link');
 				return;
 			}
 			if (!message.member.voice.channelID) {
-				console.log(message.member.channelID);
 				message.channel.send('you must be in a channel first');
 				return;
 			}
@@ -53,14 +63,20 @@ client.on('message', message => {
 			var server = servers[message.guild.id];
 
 			server.queue.push(args[1]);
-			console.log(message.guild.voice)
-			if (!message.guild.voice.channelID) {
-				console.log('here');
-				message.member.voice.channel.join().then(connection => {
-					play(connection, message);
-				})
-				
+
+			try{
+				if (!message.guild.voice) {
+					message.member.voice.channel.join().then(connection => {
+						play(connection, message);
+					})
+					
+				}
 			}
+			catch(err){
+
+				console.log(err)
+			}
+			return;
 		case 'queue':
 
 			const queue = servers[message.guild.id]['queue'].map((x, index)=> [index, x]);
@@ -68,7 +84,45 @@ client.on('message', message => {
 			message.channel.send(`current queue:\n${queue.map(song => {
 					return `[${song[0]}] : ${song[1]}\n`
 				})}
-			`)
+			`) 
+			return;
+
+		
+		case 'stop':
+			var server = servers[message.guild.id]
+			if(message.guild.voice.connection) {
+				for(var i = server.queue.length-1; i >= 0; i--) {
+					server.queue.splice(i, 1);
+				}
+				server.dispatcher.destroy();
+				message.guild.voice.connection.disconnect();
+				console.log('stopped playing');
+			}else {
+				try{
+					for(var i = server.queue.length-1; i >= 0; i--) {
+						server.queue.splice(i, 1);
+					}
+				}
+				catch(err) {
+					console.log(err);
+				}
+			}
+			return;
+
+		/*debugging commands */
+		case 'server':
+			var server = servers[message.guild.id];
+			console.log(server);
+			return;
+		case 'quit':
+			try {
+				message.guild.voice.connection.disconnect();
+				console.log('force disconnected');
+			}
+			catch(err) {
+				console.log(err);
+			}
+			return;
 	}
 
 })
